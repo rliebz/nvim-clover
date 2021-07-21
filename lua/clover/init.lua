331d94ac5e2da1770a51a24654377b38c307c693
Robert Liebowitz <rliebz@gmail.com>
2021-07-20 23:00:05 -0400

    Give uncovered statements a higher priority

--- lua/clover/init.lua
+++ lua/clover/init.lua
@@ -26,21 +26,35 @@ local function highlight(matches)
 	end
 
 	for _, match in ipairs(matches) do
-		vim.fn.matchaddpos(match.group, match.pos)
+		vim.fn.matchaddpos(match.group, match.pos, match.priority)
 	end
 end
 
---- get_matches returns a list of matches suitable for matchaddpos.
+--- get_matches returns a list of matches suitable for matchaddpos. All lines
+-- and columns provided must be 1-indexed.
 --
 -- @param start_line the first line to highlight
 -- @param start_col the optional column of the first line to highlight
 -- @param end_line the last line to highlight
 -- @param end_col the optional column of the last line to highlight
 -- @param covered whether the line is covered
+-- @return an array of tables each with a group, pos, and priority
 local function get_matches(start_line, start_col, end_line, end_col, covered)
-	local group = "CloverUncovered"
-	if covered then
-		group = "CloverCovered"
+	local group = "CloverCovered"
+	local priority = 10
+	if not covered then
+		group = "CloverUncovered"
+		-- Some reporters such as jest let statements overlap. If that's the case,
+		-- consider the uncovered statements to have a higher priority.
+		priority = 15
+	end
+
+	local function new_match(pos)
+		return {
+			group = group,
+			priority = priority,
+			pos = pos,
+		}
 	end
 
 	if not start_col or start_col == vim.NIL then
@@ -53,21 +67,21 @@ local function get_matches(start_line, start_col, end_line, end_col, covered)
 
 	if start_line == end_line then
 		local pos = { { start_line, start_col, end_col - start_col } }
-		return { { group = group, pos = pos } }
+		return { new_match(pos) }
 	end
 
 	local matches = {}
 
 	local first_length = line_length(start_line) - start_col + 1
 	local first_pos = { { start_line, start_col, first_length } }
-	table.insert(matches, { group = group, pos = first_pos })
+	table.insert(matches, new_match(first_pos))
 
 	for line = start_line + 1, end_line - 1, 1 do
-		table.insert(matches, { group = group, pos = { line } })
+		table.insert(matches, new_match({ line }))
 	end
 
 	local last_pos = { { end_line, 1, end_col - 1 } }
-	table.insert(matches, { group = group, pos = last_pos })
+	table.insert(matches, new_match(last_pos))
 
 	return matches
 end
